2449b8397221d4cfd75f460f088210ed07bc7eea
haaanah <haaanah@hotmail.com>
2015-11-30 16:41:14 +0000

    Create Xcode project

---
+++ Orienteer/Orienteer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Orienteer
+//
+//  Created by Hannah Svensson on 2015-11-30.
+//  Copyright © 2015 COMP3222. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

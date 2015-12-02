7339292e0ab1a1997b77f1978b57d73067c9b6f6
Daniel Alderman <daniel.alderman77@gmail.com>
2015-12-02 11:41:27 +0000

    Created and began to implement the getData method.
    getData method is initialised with the response from the rest weather service and parses the Json to retrieve individual fields.

--- Orienteer/Orienteer/WeatherViewController.swift
+++ Orienteer/Orienteer/WeatherViewController.swift
@@ -16,7 +16,9 @@ class WeatherViewController: UIViewController {
         let url = NSURL(string: "http://api.openweathermap.org/data/2.5/weather?lat=53.8073&lon=-1.5517&appid=2de143494c0b295cca9337e1e96b00e0&units=metric")
         
         let getRequest = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
-            print(NSString(data: data!, encoding: NSUTF8StringEncoding))
+            let response = (NSString(data: data!, encoding: NSUTF8StringEncoding))
+            print(response)
+            self.getData(response)
         }
         getRequest.resume()
         
@@ -25,6 +27,20 @@ class WeatherViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func getData(response: NSString?) {
+        let jsonData = response!.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        var locationName: String?
+        
+        if let json: NSDictionary = try! NSJSONSerialization.JSONObjectWithData(jsonData!, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary {
+            if let value = json["name"] as? String {
+                locationName = value
+            }
+        }
+        
+        print(locationName!)
     }
 
 }

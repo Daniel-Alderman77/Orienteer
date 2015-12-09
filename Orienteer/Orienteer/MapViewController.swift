fec688f5c200166b7f1e7c7f146e7db4c7b0c866
haaanah <haaanah@hotmail.com>
2015-12-09 13:58:15 +0000

    Created a new class that handles the location.

--- Orienteer/Orienteer/MapViewController.swift
+++ Orienteer/Orienteer/MapViewController.swift
@@ -9,13 +9,8 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController, CLLocationManagerDelegate {
-    
-    let requiredAccuracy: CLLocationAccuracy = 100.0
-    
-    var locManager = CLLocationManager()
-    
-    var tryingToLocate = false
+class MapViewController: UIViewController {
+    let u = UpdateLocation()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,14 +20,15 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidAppear(animated: Bool) {
         // Update location when view is open
         print("Updating Location")
+    
     }
     
     override func viewWillDisappear(animated: Bool) {
         // Stop updating location
         super.viewWillDisappear(animated)
-        locManager.startUpdatingLocation()
+        u.startLocating()
         
-        locManager.stopUpdatingLocation()
+        u.stopLocating()
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,42 +36,12 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func couldBeLocatable() -> Bool {
-        if !CLLocationManager.locationServicesEnabled() {
-            // Location services not enabled but might be in future
-            return true
-        }
-        
-        let status = CLLocationManager.authorizationStatus()
-        
-        switch status {
-            case .AuthorizedAlways, .AuthorizedWhenInUse:
-                return true
-            case .NotDetermined:
-                // Ask user for permission
-                locManager.requestWhenInUseAuthorization()
-                return true
-            case .Restricted, .Denied:
-                return false
-        }
-    }
-    
-    func getLocation(didUpdateLocations locations: [AnyObject]!)->NSArray {
-        
-        let locValue : CLLocationCoordinate2D = locManager.location!.coordinate
-        let long = locValue.longitude
-        let lat = locValue.latitude
-        let locArray = [lat, long]
-        return locArray
-        
-        
-    }
-
     // MARK: Outlets
     
     @IBOutlet weak var mapView: MKMapView! {
         didSet {
-            let locArray = getLocation(didUpdateLocations: [""])
+            let manager = u.locManager
+            let locArray = u.getLocation(manager, didUpdateLocations: [""])
             mapView.mapType = .Satellite
             mapView.pitchEnabled = false
             let location = CLLocationCoordinate2D(latitude: CLLocationDegrees(locArray[0] as! NSNumber), longitude: CLLocationDegrees(locArray[1] as! NSNumber))
---
+++ Orienteer/UpdateLocation.swift
@@ -0,0 +1,57 @@
+//
+//  UpdateLocation.swift
+//  Orienteer
+//
+//  Created by Hannah Svensson on 2015-12-09.
+//  Copyright © 2015 COMP3222. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class UpdateLocation: CLLocationManager {
+    
+    let requiredAccuracy: CLLocationAccuracy = 100.0
+    
+    var locManager = CLLocationManager()
+    
+    var tryingToLocate = false
+
+
+    func couldBeLocatable() -> Bool {
+        if !CLLocationManager.locationServicesEnabled() {
+            // Location services not enabled but might be in future
+            return true
+        }
+        
+        let status = CLLocationManager.authorizationStatus()
+        
+        switch status {
+        case .AuthorizedAlways, .AuthorizedWhenInUse:
+            return true
+        case .NotDetermined:
+            // Ask user for permission
+            locManager.requestWhenInUseAuthorization()
+            return true
+        case .Restricted, .Denied:
+            return false
+        }
+    }
+    func startLocating(){
+    
+        locManager.startUpdatingLocation()
+    }
+    
+    func stopLocating(){
+        locManager.stopUpdatingLocation()
+    }
+    
+    func getLocation(manager: CLLocationManager, didUpdateLocations locations: [AnyObject]!)->NSArray{
+        
+        let locValue : CLLocationCoordinate2D = locManager.location!.coordinate
+        let long = locValue.longitude
+        let lat = locValue.latitude
+        let locArray = [lat, long]
+        return locArray
+    }
+
+}

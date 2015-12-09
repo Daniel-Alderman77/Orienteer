4192a2a397d0e58093e5300242c8937c051afaf3
Daniel Alderman <daniel.alderman77@gmail.com>
2015-12-09 12:38:52 +0000

    Added View Object to storyboard and created CompassView class to contain code to render compass.

---
+++ Orienteer/Orienteer/CompassView.swift
@@ -0,0 +1,19 @@
+//
+//  CompassView.swift
+//  Orienteer
+//
+//  Created by Daniel Alderman on 09/12/2015.
+//  Copyright © 2015 COMP3222. All rights reserved.
+//
+
+import UIKit
+
+class CompassView: UIView {
+
+
+    override func drawRect(rect: CGRect) {
+        
+    }
+    
+
+}

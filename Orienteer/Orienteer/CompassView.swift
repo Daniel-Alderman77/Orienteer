2d8fff748384376288c3e8cbc795c5a936f7e7bf
Daniel Alderman <daniel.alderman77@gmail.com>
2015-12-10 16:14:58 +0000

    Added UIView to CompassViewController.
    Placed widgets of CompassViewController into stack views to create and preserve a layout.
    
    CompassView renders circle into newly created UIView widget.

--- Orienteer/Orienteer/CompassView.swift
+++ Orienteer/Orienteer/CompassView.swift
@@ -8,12 +8,35 @@
 
 import UIKit
 
-class CompassView: UIView {
+let π:CGFloat = CGFloat(M_PI)
 
-
-    override func drawRect(rect: CGRect) {
-        
-    }
+@IBDesignable class CompassView: UIView {
     
-
+    @IBInspectable var compassColor: UIColor = UIColor.darkGrayColor()
+    
+    override func drawRect(rect: CGRect) {
+        let center = CGPoint(x:bounds.width/2, y: bounds.height/2)
+        
+        let radius: CGFloat = max(bounds.width, bounds.height)
+        
+        // Thickness of cirlce
+        let circleWidth: CGFloat = 5
+        
+        // Start angle and end angle of circle in radians
+        let startAngle: CGFloat = 0
+        let endAngle: CGFloat = 2 * π
+        
+        // Create path
+        let path = UIBezierPath(arcCenter: center,
+            radius: radius/2 - circleWidth/2,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: true)
+        
+        path.lineWidth = circleWidth
+        compassColor.setStroke()
+        
+        // Draw path
+        path.stroke()
+    }
 }
